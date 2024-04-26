7dc708663f0f27a38b65e2e48bac3cfca9a27ae2
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-04-26 21:41:59 +0300

    feat: add Range and color variables

---
+++ src/app/components/RangeFilter/index.tsx
@@ -0,0 +1,53 @@
+import { ChangeEventHandler, useCallback } from 'react';
+
+import Input from '@/components/Input';
+
+import styles from './styles.module.scss';
+
+type RangeFilterProps = {
+  left: string;
+  right: string;
+  label: string;
+  leftPlaceholder?: string;
+  rightPlaceholder?: string;
+  onChange: (left: string, right: string) => void;
+};
+
+export default function RangeFilter({
+  left,
+  right,
+  label,
+  leftPlaceholder,
+  rightPlaceholder,
+  onChange,
+}: RangeFilterProps) {
+  const handleLeftChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
+    ({ target }) => onChange(target.value, right),
+    [onChange, right],
+  );
+
+  const handleRightChange = useCallback<ChangeEventHandler<HTMLInputElement>>(
+    ({ target }) => onChange(left, target.value),
+    [onChange, left],
+  );
+
+  return (
+    <div className={styles['wrapper']}>
+      <Input
+        type="number"
+        value={left}
+        onChange={handleLeftChange}
+        placeholder={leftPlaceholder}
+      />
+      <span>≤</span>
+      <label>{label}</label>
+      <span>≤</span>
+      <Input
+        type="number"
+        value={right}
+        onChange={handleRightChange}
+        placeholder={rightPlaceholder}
+      />
+    </div>
+  );
+}
---
+++ src/app/styles/colors.ts
@@ -0,0 +1,22 @@
+import variables from './exports.module.scss';
+
+const colors = {
+  black: variables['black'],
+  darkGray: variables['dark-gray'],
+  gray: variables['gray'],
+  lightGray: variables['light-gray'],
+  white: variables['white'],
+
+  darkBlue: variables['dark-blue'],
+  lightBlue: variables['light-blue'],
+  cyan: variables['cyan'],
+  magenta: variables['magenta'],
+  darkPurple: variables['dark-purple'],
+  purple: variables['purple'],
+  lavander: variables['lavander'],
+
+  green: variables['green'],
+  red: variables['red'],
+};
+
+export default colors;
