7da999c819da5c6f44f0e071f999b46dff322e90
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-04-26 21:07:13 +0300

    feat: add Button

---
+++ src/app/components/Button/index.tsx
@@ -0,0 +1,23 @@
+import { ButtonHTMLAttributes, FC } from 'react';
+
+import clsx from 'clsx';
+
+import styles from './styles.module.scss';
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> & {
+  'data-type'?: 'primary' | 'secondary';
+};
+
+const Button: FC<ButtonProps> = ({ children, ...props }) => {
+  return (
+    <button
+      data-type="primary"
+      {...props}
+      className={clsx(props.className, styles['button'])}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
