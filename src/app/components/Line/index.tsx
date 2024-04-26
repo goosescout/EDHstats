50c0a67a5ced922f8b1ae2da18719ca2d0d03795
Michael Gurevich <michael.gurevich.02@mail.ru>
2024-04-26 21:19:52 +0300

    feat: add Line

---
+++ src/app/components/Line/index.tsx
@@ -0,0 +1,20 @@
+import { FC } from 'react';
+
+import clsx from 'clsx';
+
+import styles from './styles.module.scss';
+
+type LineProps = {
+  className?: string;
+  top?: number;
+  bottom?: number;
+};
+
+const Line: FC<LineProps> = ({ className, top, bottom }) => (
+  <hr
+    className={clsx(className, styles['line'])}
+    style={{ marginTop: top ?? 0, marginBottom: bottom ?? 0 }}
+  />
+);
+
+export default Line;
